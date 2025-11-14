2a24b697458c6166596bce096fd0edb9a160c22d
leandro-etec-2025 <https://github.com/leandro-etec-2025/MenuRepo.git>
2025-11-14 10:43:59 -0300

    Adicionado menu final

--- src/ClassePrincipal.java
+++ src/ClassePrincipal.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ClassePrincipal {
 	
 	public static void limparTela() {
@@ -15,7 +17,38 @@ public class ClassePrincipal {
 	
 	public static void main(String[] args) {
 		
-
+		Scanner sc = new Scanner(System.in);
+		
+		int opcao;
+		
+		do{
+			
+			limparTela();
+			System.out.println("MENU DE OPÇÃO"); //sysout + ctrl + space
+			System.out.println("[1] Adicionar um aluno");
+			System.out.println("[2] Excluir um aluno");
+			System.out.println("[3] Ver alunos");
+			System.out.println("[4] SAIR");
+				
+			opcao = sc.nextInt();
+			sc.nextLine();
+			
+			limparTela();
+			
+			switch(opcao){
+			
+			case 1:
+				System.out.println("Selecionado opção 1");
+			case 2:
+				System.out.println("Selecionado opção 2");
+			case 3:
+				System.out.println("Selecionado opção 3");
+			default:
+				System.out.println("Opção Padrão");
+			}	
+			
+		} while(opcao != 4);		
+		
 	}
 
 }
